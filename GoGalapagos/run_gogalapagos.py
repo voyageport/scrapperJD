2960f0fa5c0e5db0a079c2c67085444d7c89de1f
Cristian Avila <118990339+voyageport@users.noreply.github.com>
2023-01-23 14:45:04 -0500

    Update run_gogalapagos.py

--- GoGalapagos/run_gogalapagos.py
+++ GoGalapagos/run_gogalapagos.py
@@ -8,8 +8,8 @@ import data_process
 
 import sys
     # caution: path[0] is reserved for script path (or '' in REPL)
-#sys.path.insert(1, r'C:\Users\Administrator\Documents\Projects\scrapper_JD') # Path to use in Lightsail
-sys.path.insert(1, '/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/') # Path para uso personal
+sys.path.insert(1, r'C:\Users\Administrator\Documents\Projects\scrapper_JD') # Path to use in Lightsail
+#sys.path.insert(1, '/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/') # Path para uso personal
 
 import send_information
 
