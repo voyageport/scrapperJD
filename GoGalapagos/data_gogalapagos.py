44e80d565f609f11a4d7e1db77a38ec6da74242b
Cristian Avila <118990339+voyageport@users.noreply.github.com>
2023-01-23 14:45:47 -0500

    Update data_gogalapagos.py

--- GoGalapagos/data_gogalapagos.py
+++ GoGalapagos/data_gogalapagos.py
@@ -14,16 +14,16 @@ HEADERS  = {
     }
 
 def return_boats_data():
-    #with open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\boats_ids.json', 'r') as f: # Path para Lightsail
-    with open("/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/boats_ids.json") as boats_fp:
+    with open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\boats_ids.json', 'r') as f: # Path para Lightsail
+    #with open("/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/boats_ids.json") as boats_fp:
         return json.load(boats_fp)
     
 BOATS_DATA = return_boats_data()
 
 
 def return_cabin_ids():
-    #with open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoGalapagos\cabin_types_internal.json', 'r') as f: # Path para Lightsail
-    with open("/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/GoGalapagos/cabin_types_internal.json") as cabins_fp:
+    with open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoGalapagos\cabin_types_internal.json', 'r') as f: # Path para Lightsail
+    #with open("/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/GoGalapagos/cabin_types_internal.json") as cabins_fp:
         return json.load(cabins_fp)
     
     
