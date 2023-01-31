28e8ea6383fd212cfd393d69ca7342f01ee07edb
Cristian Avila <118990339+voyageport@users.noreply.github.com>
2023-01-31 17:49:28 -0500

    Add files via upload

--- send_information.py
+++ send_information.py
@@ -4,8 +4,8 @@ from requests.auth import HTTPBasicAuth
 import sys
     
 
-sys.path.insert(1, r'C:\Users\Administrator\Documents\Projects\scrapper_JD') # Path para Lightsail
-#sys.path.insert(1, '/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final') # Path para test personal
+#sys.path.insert(1, r'C:\Users\Administrator\Documents\Projects\scrapper_JD') # Path para Lightsail
+sys.path.insert(1, '/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final') # Path para test personal
 
 import API_CREDENTIALS
 
@@ -21,8 +21,8 @@ with open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Contr
 
 def update_price_and_availability_golden(boat_id=None, cabin_type_id=None):
     
-    file_json = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoldenGalapagos\Requests\request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'r') # Código para Lightsail
-    #file_json = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/GoldenGalapagos/Requests/request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'r') # Para uso personal
+    #file_json = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoldenGalapagos\Requests\request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'r') # Código para Lightsail
+    file_json = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/GoldenGalapagos/Requests/request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'r') # Para uso personal
     data = file_json.read()
     
     #print(type(data))
@@ -62,8 +62,8 @@ def send_information_golden(final_json):
             #print(info_in_json)
     
     
-            f = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoldenGalapagos\Requests\request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'w') # Path to use in Lightsail 
-            #f = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/GoldenGalapagos/Requests/request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'w')
+            #f = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoldenGalapagos\Requests\request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'w') # Path to use in Lightsail 
+            f = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/GoldenGalapagos/Requests/request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'w')
             f.write(json.dumps(info_in_json))
             f.close()
     
@@ -78,8 +78,8 @@ def send_information_golden(final_json):
 def update_price_and_availability(boat_id=None, cabin_type_id=None, specific_path=''):
     print('En update price: ', specific_path)
 
-    file_json = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\{}\Requests\request_boat_{}_cabin_type_{}.json'.format(specific_path, boat_id, cabin_type_id), 'r') # Código para Lightsail
-    #file_json = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/{}/Requests/request_boat_{}_cabin_type_{}.json'.format(specific_path, boat_id, cabin_type_id), 'r') # Para uso personal
+    #file_json = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GoldenGalapagos\Requests\request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'r') # Código para Lightsail
+    file_json = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/{}/Requests/request_boat_{}_cabin_type_{}.json'.format(specific_path, boat_id, cabin_type_id), 'r') # Para uso personal
     data = file_json.read()
     
     #print(type(data))
@@ -87,7 +87,7 @@ def update_price_and_availability(boat_id=None, cabin_type_id=None, specific_pat
     data = json.loads(data) 
     
     
-    #print(data)
+    #print(data) # Prints the data that is being sent to the GDS
     '''
     Code to manually transform the 'departures' list to string
     '''
@@ -113,24 +113,46 @@ def send_information(final_json, specific_path):
     for boat_id in final_json:
         for cabin_type_id in final_json[boat_id]:
 
+            """
+            Orders the departure dates
+            """
             info_in_json = final_json[boat_id][cabin_type_id]
-            list_string = json.dumps(info_in_json['departures'])
+            departures_sorted = sorted(final_json[boat_id][cabin_type_id]['departures'], key=lambda d: d['departure_date'])
             
+            #print(departures_sorted, end = '\n\n')  # Prints only the 'departures' list (sorted)
+            
+            # Converts the departures list onto string format
+            info_in_json['departures'] = departures_sorted
+            list_string = json.dumps(info_in_json['departures'])
             info_in_json['departures'] = list_string
             
-            #print(info_in_json)
+            ##print(info_in_json, end = '\n\n*\n\n')
             
-            f = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\{}\Requests\request_boat_{}_cabin_type_{}.json'.format(specific_path, boat_id, cabin_type_id), 'w') # Path to use in Lightsail 
-            #f = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/{}/Requests/request_boat_{}_cabin_type_{}.json'.format(specific_path, boat_id, cabin_type_id), 'w')
+            f = open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\GAdventures\Requests\request_boat_{}_cabin_type_{}.json'.format(boat_id, cabin_type_id), 'w') # Path to use in Lightsail 
+            f = open('/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/{}/Requests/request_boat_{}_cabin_type_{}.json'.format(specific_path, boat_id, cabin_type_id), 'w')
             f.write(json.dumps(info_in_json))
             f.close()
+            
+            
     
             update_price_and_availability(boat_id, cabin_type_id, specific_path)
+           
+           
+           
+           
     
 
-#update_price_and_availability(48, 176, 'GAdventures')
+
+
+
+
 #update_price_and_availability(31, 240, 'GoldenGalapagos')
+#update_price_and_availability(48, 177, 'GAdventures')
 #update_price_and_availability(52, 313, 'GoGalapagos')
+
+
+
+
 
 
 
