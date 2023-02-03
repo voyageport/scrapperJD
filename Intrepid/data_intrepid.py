bb747998912037ee3230e2bd24b3673f8d420c1d
Cristian Avila <118990339+voyageport@users.noreply.github.com>
2023-02-03 10:24:10 -0500

    Add files via upload

---
+++ Intrepid/data_intrepid.py
@@ -0,0 +1,85 @@
+import json
+
+DRIVER_PATH_WINDOWS = r"C:\SeleniumDrivers" # For use in Lightsail
+DRIVER_PATH_PERSONAL = '/Users/juandiegovaca/Downloads' # For personal use
+
+URL = 'https://www.intrepidtravel.com/en'
+
+CLOSE_COOKIES_CLASS_NAME = 'onetrust-close-btn-handler onetrust-close-btn-ui banner-close-button ot-close-icon'.replace(' ', '.')
+
+SEARCH_BOX_PATH = '/html/body/div[1]/header/div/div[2]/div[2]/div/div/div/div/div[1]/div/div/input'
+
+CHECK_BOX_ADVENTURE_CRUISING_PATH = '/html/body/div[1]/div/div/div[4]/div[2]/div[5]/div[2]/div[2]/div'
+
+NUMBER_OF_TRIPS_PATH = '/html/body/div[1]/div/div/div[3]/div/span/strong'
+
+#INDIVIDUAL_TRIP_CARD_CLASS_NAME = 'l-grid__cell l-grid__cell--flex l-grid__cell--12-col l-grid__cell--6-col-desktop l-grid__cell--4-col-tablet'.replace(' ', '.')
+
+INDIVIDUAL_TRIP_CARD_CLASS_NAME = 'card__content card__content--link'.replace(' ', '.')
+
+LINKS_TO_VISIT = []
+
+NEXT_PAGE_PATH = '/html/body/div[1]/div/div/div[5]/div[3]/div[4]/button'
+                  
+CLOSE_COOKIES_2_PATH = '/html/body/div[4]/div[3]/div/div[2]/button'
+
+CLOSE_COOKIES_2_CLASS_NAME = 'onetrust-close-btn-handler onetrust-close-btn-ui banner-close-button ot-close-icon'.replace(' ', '.')
+
+DATE_SELECTOR_CLASS_NAME = 'btn btn-month-selector date ng-binding'.replace(' ', '.')
+
+TOTAL_DAYS_CLASS_NAME = 'product-info__content'
+
+TOUR_TITLE_CLASS_NAME = 'banner__heading'
+
+START_CITY_PATH = '/html/body/div[1]/div/section/div[4]/div[1]/dl/dd[1]'
+
+FINISH_CITY_PATH = '/html/body/div[1]/div/section/div[4]/div[1]/dl/dd[2]'
+
+VIEW_MORE_DEPARTURES_CLASS_NAME = 'btn btn-lg btn-block btn-passive'.replace(' ', '.')
+
+VIEW_MORE_DEPARTURES_PATH = '/html/body/div[1]/div/section/div[12]/div/div/div[2]/div/div/div/div[3]/button'
+
+#INDIVIDUAL_DATES_INFO_CLASS_NAME = 'info-date collapsed'.replace(' ', '.')
+INDIVIDUAL_DATES_INFO_CLASS_NAME = 'info-date'
+INDIVIDUAL_DATES_INFO_CSS = "[id^='panel-id'"
+
+INDIVIDUAL_DATES_PATH = '/html/body/div[1]/div/section/div[12]/div/div/div[2]/div/div/div/div[2]/div[{}]/div[1]'
+
+
+TOTAL_NUMBER_OF_DAYS = 0
+
+DEPARTURES_DATES_LIST = []
+
+ARRIVAL_DATES_LIST = []
+
+POP_UP_INTRUSIVE_PATH = '/html/body/div[2]/div/div/div[2]/div[1]/button'
+
+CABIN_TYPES_PATH_GENERAL = '/html/body/div[1]/div/section/div[12]/div/div/div[2]/div/div/div/div[2]/div[{}]/div[2]/div[{}]/div/div/div/div[1]/h4'
+
+PRICES_PATH_GENERAL = '/html/body/div[1]/div/section/div[12]/div/div/div[2]/div/div/div/div[2]/div[{}]/div[2]/div[{}]/div/div/div/div[2]'
+
+AVAILABILITIES_PATH_GENERAL = '/html/body/div[1]/div/section/div[12]/div/div/div[2]/div/div/div/div[2]/div[{}]/div[2]/div[{}]/div/div/div/div[3]'
+
+COMPLETE_JASON = {}
+
+def return_boats_data():
+    #with open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\boats_ids.json', 'r') as boats_fp: # Path para Lightsail
+    with open("/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/boats_ids.json") as boats_fp:
+        return json.load(boats_fp)
+    
+BOATS_DATA = return_boats_data()
+
+def return_cabin_ids():
+    #with open(r'C:\Users\Administrator\Documents\Projects\scrapper_JD\Intrepid\internal_cabins_intrepid.json', 'r') as cabins_fp: # Path para Lightsail
+    with open("/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/Intrepid/internal_cabins_intrepid.json") as cabins_fp: # Path para uso personal
+        return json.load(cabins_fp)
+
+CABINS_IDS_INTERNAL = return_cabin_ids()
+
+
+
+
+
+
+
+
---
+++ Intrepid/run_intrepid.py
@@ -0,0 +1,120 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import scrapper_intrepid
+import data_intrepid
+import requests
+import string
+import re
+import time
+import json
+
+import sys
+    # caution: path[0] is reserved for script path (or '' in REPL)
+#sys.path.insert(1, r'C:\Users\Administrator\Documents\Projects\scrapper_JD') # Path to use in Lightsail
+sys.path.insert(1, '/Users/juandiegovaca/Desktop/Voyageport/Screen Scraping/Version Control/Final/') # Path para uso personal
+
+import send_information
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+
+#driver_path = data_gadventures.DRIVER_PATH_WINDOWS # Para Lightsail
+driver_path = data_intrepid.DRIVER_PATH_PERSONAL # Para uso personal
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+print('Starting scraping process for Intrepid')
+
+
+driver.get(data_intrepid.URL)
+#driver.maximize_window()
+print('Scraping for Intrepid starts')
+time.sleep(3)
+
+
+# Click on 'X' Cookies
+scrapper_intrepid.click_on_element_by_class_name(driver, data_intrepid.CLOSE_COOKIES_CLASS_NAME)
+
+# Search for 'Galapagos Islands'
+scrapper_intrepid.write_element_by_xpath(driver, data_intrepid.SEARCH_BOX_PATH, 'Galapagos Islands')
+
+# Click on 'Adventure Cruising'
+scrapper_intrepid.click_on_element_by_path(driver, data_intrepid.CHECK_BOX_ADVENTURE_CRUISING_PATH)
+print("Filtered for 'Adventure Cruising'")
+time.sleep(3)
+
+# Finds the number of trips displayed on the page
+total_trips_web = int(driver.find_element(By.XPATH, data_intrepid.NUMBER_OF_TRIPS_PATH).get_attribute('innerHTML'))
+print('Number of trips found (web): ', total_trips_web)
+
+# Stores all the links
+scrapper_intrepid.get_all_tour_links(driver, total_trips_web)
+print('All URLs stored')
+time.sleep(2)
+
+
+# Get all data
+print('Getting all data...')
+scrapper_intrepid.get_data(driver)
+print('\n\nScraping process finished succesfuly\n\n')
+time.sleep(3)
+driver.close()
+
+# Send info to API
+print('Sending info to API...')
+send_information.send_information(data_intrepid.COMPLETE_JASON, 'Intrepid')
+
+print('\n\n***Process finished succesfuly')
+
+
+
+
+
+
+"""
+CODE FOR ONLY TESTING 1 LINK
+
+
+scrapper_intrepid.get_data(driver)
+print('Scraping process ended')
+#time.sleep(2)
+print(data_intrepid.COMPLETE_JASON)
+#print('Sending information to API...\n')
+#send_information.send_information(data_intrepid.COMPLETE_JASON, 'Intrepid')
+"""
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
